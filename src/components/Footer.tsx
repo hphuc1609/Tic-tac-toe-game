239c9af332f7266a8eb76425a4ff7bf502dbb5df
LT. Hoang Phuc <luuboy91@gmail.com>
2025-02-14 10:21:45 +0700

    feat: add footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className='text-center text-gray-500 text-sm'>
+      <p>Copyright &copy; 2025 LT. Hoang Phuc</p>
+    </footer>
+  )
+}
+
+export default Footer
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,4 @@
+import Footer from '@/components/Footer'
 import Logo from '@/components/Logo'
 import { AppContext } from '@/context/appContext'
 import hoverSound from '@/sound/mixkit-cool-interface-click-tone-2568.wav'
@@ -31,61 +32,66 @@ const Home = () => {
   }
 
   return (
-    <motion.main
-      animate={{ opacity: 1, y: 0 }}
-      initial={{ opacity: 0, y: -20 }}
-      transition={{ duration: 1, delay: 0.2 }}
-      exit={{ opacity: 0, y: -20 }}
-      className='m-auto min-w-[300px] flex flex-col items-center'
-    >
-      <Logo />
-      <button
-        onMouseEnter={() => playHover()}
-        onClick={handleSingleplayer}
-        className='button-3d button-3d-click flex items-center justify-center gap-2 w-full h-16 !px-5 bg-primary uppercase tracking-widest font-semibold text-xl !shadow-[0_8px_0_#0284c7] active:!shadow-[0_4px_0_#0284c7] hover:bg-sky-400 mt-9 mb-7'
+    <div className='flex flex-col'>
+      <motion.main
+        animate={{ opacity: 1, y: 0 }}
+        initial={{ opacity: 0, y: -20 }}
+        transition={{ duration: 1, delay: 0.2 }}
+        exit={{ opacity: 0, y: -20 }}
+        className='m-auto min-w-[300px] flex flex-col items-center justify-around gap-10'
       >
-        <svg
-          xmlns='http://www.w3.org/2000/svg'
-          width='24'
-          height='24'
-          viewBox='0 0 24 24'
-          fill='none'
-          stroke='currentColor'
-          strokeWidth='2'
-          strokeLinecap='round'
-          strokeLinejoin='round'
-          className='lucide lucide-user'
-        >
-          <path d='M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2' />
-          <circle cx='12' cy='7' r='4' />
-        </svg>
-        New Game
-      </button>
-      <button
-        onMouseEnter={() => playHover()}
-        onClick={handleMultiplayer}
-        className='button-3d button-3d-click flex items-center justify-center gap-2 w-full h-16 !px-5 bg-secondary uppercase tracking-widest font-semibold text-xl !shadow-[0_8px_0_#ca8a04] active:!shadow-[0_4px_0_#ca8a04] hover:bg-yellow-400'
-      >
-        <svg
-          xmlns='http://www.w3.org/2000/svg'
-          width='24'
-          height='24'
-          viewBox='0 0 24 24'
-          fill='none'
-          stroke='currentColor'
-          strokeWidth='2'
-          strokeLinecap='round'
-          strokeLinejoin='round'
-          className='lucide lucide-users'
-        >
-          <path d='M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2' />
-          <circle cx='9' cy='7' r='4' />
-          <path d='M22 21v-2a4 4 0 0 0-3-3.87' />
-          <path d='M16 3.13a4 4 0 0 1 0 7.75' />
-        </svg>
-        Multiplayer
-      </button>
-    </motion.main>
+        <Logo />
+        <div className='w-full flex flex-col gap-7'>
+          <button
+            onMouseEnter={() => playHover()}
+            onClick={handleSingleplayer}
+            className='button-3d button-3d-click flex items-center justify-center gap-2 w-full h-16 !px-5 bg-primary uppercase tracking-widest font-semibold text-xl !shadow-[0_8px_0_#0284c7] active:!shadow-[0_4px_0_#0284c7] hover:bg-sky-400'
+          >
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              width='24'
+              height='24'
+              viewBox='0 0 24 24'
+              fill='none'
+              stroke='currentColor'
+              strokeWidth='2'
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              className='lucide lucide-user'
+            >
+              <path d='M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2' />
+              <circle cx='12' cy='7' r='4' />
+            </svg>
+            New Game
+          </button>
+          <button
+            onMouseEnter={() => playHover()}
+            onClick={handleMultiplayer}
+            className='button-3d button-3d-click flex items-center justify-center gap-2 w-full h-16 !px-5 bg-secondary uppercase tracking-widest font-semibold text-xl !shadow-[0_8px_0_#ca8a04] active:!shadow-[0_4px_0_#ca8a04] hover:bg-yellow-400'
+          >
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              width='24'
+              height='24'
+              viewBox='0 0 24 24'
+              fill='none'
+              stroke='currentColor'
+              strokeWidth='2'
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              className='lucide lucide-users'
+            >
+              <path d='M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2' />
+              <circle cx='9' cy='7' r='4' />
+              <path d='M22 21v-2a4 4 0 0 0-3-3.87' />
+              <path d='M16 3.13a4 4 0 0 1 0 7.75' />
+            </svg>
+            Multiplayer
+          </button>
+        </div>
+      </motion.main>
+      <Footer />
+    </div>
   )
 }
 
